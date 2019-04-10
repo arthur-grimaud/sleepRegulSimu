3b446d7560d13b9c09448e8665db064f18badee5
Amelie Gruel <ameliemgruel@gmail.com>
2019-04-10 17:00:18 +0200

    fichier paramètre et début du script lisant le fichier paramètres

---
+++ read_parameters.py
@@ -0,0 +1,22 @@
+#!bin/python
+#-*-coding:utf-8-*-
+
+modele = []
+fic = open("parameters.txt","r")
+lines = fic.read().split("\n")
+l = 0
+while l < len(lines) :
+    if len(lines[l]) != 0 and lines[l][0] != "/" :
+        if lines[l][0] == "*" :
+            while 
+    l+=1 
+
+fic.close()
+
+
+# name="toto" 
+# f=file('photo/'%s'.jpeg', 'wb') % name 
+
+# f=file('photo/%s.jpeg' % name, 'wb') 
+ 
+#création de classes pour chaque objet 
