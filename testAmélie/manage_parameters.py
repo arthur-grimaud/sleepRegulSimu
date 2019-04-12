ed69383469089942859b9b5e0b8f7140345a015b
amygruel <43147713+amygruel@users.noreply.github.com>
2019-04-12 15:30:09 +0200

    update write_parameters()
    write_parameters() can now handle objects

--- testAmélie/manage_parameters.py
+++ testAmélie/manage_parameters.py
@@ -13,8 +13,7 @@ def read_parameters() :
     cycles = {}
     simulation_parameters = {}
 
-    listPopulations = []
-    concentrations = {}
+    connections = {}
 
     # when the check is equal to 1, you're currently in the corresponding block (a popultion block, a concentration block, etc) 
     check_pop = 0   # check wether you're currently in a population block
@@ -30,7 +29,6 @@ def read_parameters() :
                 currentPopulation = line[3]
                 populations[currentPopulation] = {}
                 populations[currentPopulation]["name"] = currentPopulation
-                concentrations[currentPopulation] = []
             elif line[0] == "*" and check_pop == 1 :
                 check_pop = 0
             elif line[0] == "@" and check_conc == 0 :
@@ -61,7 +59,7 @@ def read_parameters() :
                 else :
                     populations[currentPopulation][line[0]] = line[2]
                 if line[0] == "pop_ext" :
-                    concentrations[currentPopulation].append(myParameter)
+                    connections[currentPopulation] = myParameter
             elif check_conc == 1 and line[0] != "@" :
                 concentrations[currentConcentration][line[0]] = line[2]
             elif check_cycle == 1 and line[0] != "+" :
@@ -70,42 +68,48 @@ def read_parameters() :
                 simulation_parameters[line[0]] = line[2]
 
     fic.close()
-    return [populations,concentrations,cycles,simulation_parameters,concentrations]
+    return populations,concentrations,cycles,simulation_parameters,connections
 
 
-def write_parameters(name_file,populations,concentrations,cycles,simulation_parameters) :
+def write_parameters(name_file,modele) :
     ### writes a file with the input parameters
     # creates a file following the same format as default_parameters.txt
 
     fic = open(name_file,"w")
 
-    for myPop in populations :
-        fic.write("* population = "+myPop+"\n")
-        for parameter in populations[myPop] :
-            if parameter == "g" or parameter == "pop_ext" or parameter == "beta" :
-                fic.write(parameter+" =")
-                for i in range(len(populations[myPop][parameter])) :
-                    fic.write(" "+populations[myPop][parameter][i])
-                fic.write("\n")
-            else : 
-                fic.write(parameter+" = "+populations[myPop][parameter]+"\n")
+    for myPop in modele['populations'] :
+        fic.write("* population = "+myPop.name+"\n")
+        myConcentration = {}
+        myConcentration["population"] = myPop.name
+        for parameter in vars(myPop) :
+            print(getattr(myPop,parameter))
+            if parameter in ["concentration","C","gamma","tau_NT"]:
+                myConcentration[parameter]=getattr(myPop,parameter)
+            else :
+                if isinstance(getattr(myPop,parameter),list) :
+                    fic.write(parameter+" =")
+                    for value in getattr(myPop,parameter) :
+                        fic.write(" "+value)
+                    fic.write("\n")
+                elif parameter != 'name' : 
+                    fic.write(parameter+" = "+getattr(myPop,parameter)+"\n")
         fic.write("*\n\n")
 
-    for myConcentration in concentrations : 
-        fic.write("@ concentration = "+myConcentration+"\n")
-        for parameter in concentrations[myConcentration] :
-            fic.write(parameter+" = "+concentrations[myConcentration][parameter]+"\n")
+        fic.write("@ concentration = "+myConcentration["concentration"]+"\n")
+        for parameter in myConcentration : 
+            if parameter != 'concentration':
+                fic.write(parameter+" = "+myConcentration[parameter]+"\n")
         fic.write("@\n\n")
 
-    for myCycle in cycles : 
-        fic.write("+ cycle = "+myCycle+"\n")
-        for parameter in cycles[myCycle] :
-            fic.write(parameter+" = "+cycles[myCycle][parameter]+"\n")
+    for myCycle in modele['cycles'] : 
+        fic.write("+ cycle = "+myCycle.name+"\n")
+        for parameter in vars(myCycle) :
+            fic.write(parameter+" = "+getattr(myCycle,parameter)+"\n")
         fic.write("+\n\n")
     
     fic.write("#\n")
-    for parameter in simulation_parameters :
-        fic.write(parameter+" = "+simulation_parameters[parameter]+"\n")
+    for parameter in modele['simulation_parameters'] :
+        fic.write(parameter+" = "+modele['simulation_parameters'][parameter]+"\n")
     fic.write("#\n")
 
     fic.close()
