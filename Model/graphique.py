ebe68a2b349138d4471618a2403616702b9211e7
Amelie Gruel <ameliemgruel@gmail.com>
2019-04-25 16:51:26 +0200

    add legends on the axes and placed homeostaic sleep drive with the concentrations

--- Model/graphique.py
+++ Model/graphique.py
@@ -2,40 +2,74 @@
 #-*-coding:utf-8-*-
 
 import matplotlib.pyplot as plt
+from pylab import *
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+import csv
 
+def readCSV(file) :
+    tmp = open(file,'r')
+    names=tmp.readline().rsplit()
+    tmp.close()
+    
+    results={}
+    for n in names : 
+        results[n]=[]
 
+    with open(file) as f:
+        reader = csv.DictReader(f, delimiter='\t')
+        for row in reader :
+            for (name,element) in row.items() :
+                results[name].append(element)
+    return results
 
-def createGraph(network):
+def createGraph(data):
 
     # plot
     plt.figure(1)
 
     sub1=plt.subplot(3,1,1)
-    plt.plot(network.results[0],network.results[1],'black')
+    plt.plot(data[0],data[1],'black')
     plt.ylim(-0.5,1.5)
+    xticks([1800,3600,5400,7200],[0.5,1,1.5,2])
+    yticks([0,0.5,1],['NREM','REM','Wake'])
     plt.xlabel('Time (h)')
-    plt.ylabel('Hypnogramme')
+    plt.ylabel('Hypnogram')
 
     sub2=plt.subplot(3,1,2)
-    sub2=plt.plot(network.results[0],network.results[2],'g',label="F_W")
-    sub2=plt.plot(network.results[0],network.results[4],'r',label="F_N")
-    sub2=plt.plot(network.results[0],network.results[6],'b',label="F_R")
-    sub2=plt.plot(network.results[0],network.results[8],'y',label="h")
+    sub2=plt.plot(data[0],data[2],'g',label="F_W")
+    sub2=plt.plot(data[0],data[4],'r',label="F_N")
+    sub2=plt.plot(data[0],data[6],'b',label="F_R")
     plt.ylim(0,7)
+    xticks([1800,3600,5400,7200],[0.5,1,1.5,2])
     plt.xlabel('Time (h)')
     plt.ylabel('Activity (Hz)')
     plt.legend(loc='best')
-    # plt.setp([0,0.5,1],'yticklabel',{'REM','NREM','wake'})
 
     sub3=plt.subplot(3,1,3)
-    sub3=plt.plot(network.results[0],network.results[3],'g',label="C_E")
-    sub3=plt.plot(network.results[0],network.results[5],'r',label="C_G")
-    sub3=plt.plot(network.results[0],network.results[7],'b',label="C_A")
+    sub3=plt.plot(data[0],data[3],'g',label="C_E")
+    sub3=plt.plot(data[0],data[5],'r',label="C_G")
+    sub3=plt.plot(data[0],data[7],'b',label="C_A")
+    sub3=plt.plot(data[0],data[8],'y',label="h")
     plt.ylim(0,1)
+    xticks([1800,3600,5400,7200],[0.5,1,1.5,2])
     plt.xlabel("Time (h)")
     plt.ylabel("Concentrations")
     plt.legend(loc='best')
 
     plt.show()
+
+
+results = readCSV('results.csv')
+data = [
+    results['time'],
+    results['hypnogram'],
+    results['wake_F'],
+    results['wake_C'],
+    results['NREM_F'],
+    results['NREM_C'],
+    results['REM_F'],
+    results['REM_C'],
+    results['H']
+]
+createGraph(data)
